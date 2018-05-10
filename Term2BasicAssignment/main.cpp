3f1655dd223ccb2242264240110a1c4e1d304b63
Dikozda <Epicgames@ofwa.net>
2018-05-10 10:59:52 +1000

    added some basic includes and functions that will be needed.

--- Term2BasicAssignment/main.cpp
+++ Term2BasicAssignment/main.cpp
@@ -1,3 +1,34 @@
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <vector>
+#include <csignal>
 
+using namespace std;
+
+void SignalHandleTerm(int signum)
+{
+
+}
+
+void MapGen()
+{
+
+}
+
+void SortArray(vector<vector<int>> Map)
+{
+
+}
+
+int main()
+{
+
+	vector<vector<int>>;
+
+	signal(SIGTERM, SignalHandleTerm);
+
+
+
+	return(0);
+}
